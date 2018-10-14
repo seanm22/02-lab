99c5c2614c0ab6304f57327e2e75d09baa21d3cf
Dave Everitt <deveritt@innotts.co.uk>
2018-10-14 20:19:42 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const hello = document.getElementById("hello");
+
+//console.log(hello);
+
+function greeting() {
+  alert("Hello!");
+  hello.textContent = "Bye";
+}
+
+hello.addEventListener("click", greeting);
+
+/*
+ for textContent vs innerHTML vs innerText see:
+ https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#Differences_from_innerText
+ */
